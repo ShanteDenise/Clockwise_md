10c5d5991378f3898db0b98199f5c328c415771e
RaFl91 <Terrell.Flemings@gmail.com>
2018-12-12 15:31:45 -0500

    getting project to run on server

--- client/src/components/Reservation.js
+++ client/src/components/Reservation.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 
 export default class Reservation extends Component {
+  state = {
+    reservations: [{}]
+  }
+
+  getAllUserReservations = () => {
+    axios.get()
+  }
   render() {
     return (
       <div>
--- main/views.py
+++ main/views.py
@@ -11,9 +11,11 @@ class ClinicView(viewsets.ModelViewSet):
     queryset = Clinic.objects.all()
     serializer_class = ClinicSerializer
 
+
 class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class ReservationView(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
