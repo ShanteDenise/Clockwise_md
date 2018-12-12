fbdefe4ac258e09ad658e4255de5d3689e5b58d8
Shante <shante.austin19@gmail.com>
2018-12-12 10:36:00 -0500

    Added views and urls

--- main/urls.py
+++ main/urls.py
@@ -1,2 +1,15 @@
+
+from django.urls import path, include
+from rest_framework import routers
+
+from . import views
+
+router = routers.DefaultRouter()
+router.register('clinics', views.ClinicViewView)
+router.register('users', views.UserViewView)
+router.register('reservations', views.ResevationView)
+
+
 urlpatterns = [
+    path('', include(router.urls))
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+
+from .serializers import ClinicSerializer, ReservationSerializer, UserSerializer
+from .models import User, Clinic, Reservation
+
+
+class ClinicView(viewsets.ModelViewSet):
+    queryset = Clinic.objects.all()
+    serializer_class = ClinicSerializer
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ReservationView(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
