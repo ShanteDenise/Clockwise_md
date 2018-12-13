a9a78c18240ccd26f7b7e9a87efdad4d4db69b6f
BrittFromIT <Brittanylewis32@gmail.com>
2018-12-13 12:11:10 -0500

    Updating User Model

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 class User extends Component {
+
+    state = {
+        clinics: (),
+        reservations: [],
+
+    }
+
+    componentDidMount() {
+        const userId = this.props.match.params.id;
+        this.fetchUser(userId)
+    }
+
+    fetchUser = async (userId) => {
+        try {
+            const userResponse = await axios.get('')
+            await this.setState([
+                clinics: clinicResponse.data,
+                reservations: reservationsResponse.data.reservations,
+            ])
+        }
+    }
     render() {
         return (
             <div>
