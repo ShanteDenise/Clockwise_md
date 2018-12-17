478ca783608e24b1a08aee8cbd194b1d070a951a
RaFl91 <Terrell.Flemings@gmail.com>
2018-12-17 11:54:49 -0500

    ReservationForm complete (init)

--- client/src/components/ReservationForm.js
+++ client/src/components/ReservationForm.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import Axios from 'axios';
+import axios from 'axios';
 
 // Used logic from Idea Board 2
 
 export default class ReservationForm extends Component {
     state = {
+        reservations: [],
         form_items: {
             reason_for_visit: '',
             date_of_visit: ''
@@ -21,12 +22,23 @@ export default class ReservationForm extends Component {
     handleSubmit = (event) => {
         axios.post('/api/reservations', this.state.reserveForm).then(res => {
             console.log(res.data)
+            this.props.history(`/reservations/${res.data._id}`)
         })
     }
     render() {
         return (
             <div>
-
+                <form onSubmit={this.handleSubmit} >
+                    <div>
+                        <label htmlFor="reason_for_visit">Reason for Visit </label>
+                        <input onChange={this.handleChange} value={this.state.form_items.reason_for_visit} type="text" name="reason for visit" />
+                    </div>
+                    <div>
+                        <label htmlFor="date_of_visit">Date of Visit</label>
+                        <input onChange={this.handleChange} value={this.state.form_items.date_of_visit} type="text" name="date of visit" />
+                    </div>
+                    <button type="submit">Next</button>
+                </form>
             </div>
         )
     }
