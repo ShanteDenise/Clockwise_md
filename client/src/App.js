e702833ef334116f24f3d4b6f98448f36c9f7aec
BrittFromIT <Brittanylewis32@gmail.com>
2018-12-17 12:24:59 -0500

    Creating User Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Clinic from './components/Clinic';
-<<<<<<< HEAD
-import User from './components/User'
-=======
 import Confirmation from './components/Confirmation'
->>>>>>> 205eb3f4ff2d9c99d40a04342d2a4871bd16f372
 import './App.css';
 
 
@@ -18,13 +14,9 @@ class App extends Component {
           <Switch>
 
           <Route exact path="/clinic" component={ Clinic } />
-<<<<<<< HEAD
-          <Route exact path="/user" component={User} />
-=======
           <Route exact path="/clinic/:id" component={ Confirmation }/>
           <Link to="/clinic">Reserve My Spot!</Link>
 
->>>>>>> 205eb3f4ff2d9c99d40a04342d2a4871bd16f372
 
           </Switch> 
        </Router>
